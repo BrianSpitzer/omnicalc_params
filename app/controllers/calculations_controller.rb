b9a2f0369162df00c4c261f6b678958b8d2815f8
Brian Spitzer <bspitzer@chicagobooth.edu>
2017-07-13 02:00:47 +0000

    squares

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,9 +1,9 @@
 class CalculationsController < ApplicationController
     def flex_square
-        # The incoming parameters for this action look like {"a_number"=>"5"}
+        # The incoming parameters for this action look like {"number"=>"5"}
         # Rails stores that hash in a variable called params
     
-      @user_number = params["a_number"].to_i
+      @user_number = params["number"].to_i
       
       @squared_number = @user_number**2
         
@@ -12,15 +12,41 @@ class CalculationsController < ApplicationController
     end
     
     def flex_square_root
-        # The incoming parameters for this action look like {"a_number"=>"5"}
+        # The incoming parameters for this action look like {"number"=>"5"}
         # Rails stores that hash in a variable called params
     
-      @user_number = params["a_number"].to_i
+      @user_number = params["number"].to_i
       
       @square_root_of_number = @user_number**0.5
         
       render("calculations/flexible_square_root_template.html.erb")
     
+    end
+
+    def flex_payment
+      @apr = params["basis_points"].to_f / 10000
+      @years = params["number_of_years"].to_i
+      @principal = params["present_value"].to_f
+    
+      monthly_rate = @apr / 12 
+      number_of_payments = @years * 12
+      @monthly_payment = @principal * monthly_rate * (1 + monthly_rate)**number_of_payments / (((1+monthly_rate)**number_of_payments)-1)
+    
+      render("calculations/flexible_payment_template.html.erb")
+    end
+    
+    def form_square
+    
+      render("calculations/form_square_template.html.erb")
+    end
+    
+    def form_square_result
+      @user_number = params["user_number"].to_i
+      
+      @squared_number = @user_number**2
+        
+
+      render("calculations/form_square_result_template.html.erb")
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
-  get("/flexible/square/:a_number", { :controller => "calculations", :action => "flex_square"})
-  get("/flexible/square_root/:a_number", { :controller => "calculations", :action => "flex_square_root"})
+  get("/flexible/square/:number", { :controller => "calculations", :action => "flex_square"})
+  get("/flexible/square_root/:number", { :controller => "calculations", :action => "flex_square_root"})
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculations", :action => "flex_payment"})
+  
+  get("/square/new", { :controller => "calculations", :action => "form_square"})
+  get("/square/results", { :controller => "calculations", :action => "form_square_result"})
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
